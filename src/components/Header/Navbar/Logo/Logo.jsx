0ecf58644d2344410300ea7bc67acd7761ca99e2
Md Noble <mdnoble.wd@gmail.com>
2023-09-25 12:30:56 +0600

    added navbar section styles and media queries

---
+++ src/components/Header/Navbar/Logo/Logo.jsx
@@ -0,0 +1,9 @@
+const Logo = () => {
+    return (
+        <div>
+            <img className="w-32 md:w-40 lg:w-52" src="/src/assets/Logo.png" alt="" />
+        </div>
+    );
+};
+
+export default Logo;
--- src/components/Header/Navbar/Navbar.jsx
+++ src/components/Header/Navbar/Navbar.jsx
@@ -1,32 +1,119 @@
 import { NavLink } from "react-router-dom";
+import { BsViewList } from "react-icons/bs";
+import Logo from "./Logo/Logo";
 
 const Navbar = () => {
   return (
-    <nav>
-      <div>
-        <ul>
-          <li>
-            <NavLink
-            to="/"
-            className={({ isActive, isPending }) =>
-              isPending ? "pending" : isActive ? "text-[#FF444A] underline" : ""
-            }>Home</NavLink>
-          </li>
-          <li>
-            <NavLink
-            to="/donation"
-            className={({ isActive, isPending }) =>
-              isPending ? "pending" : isActive ? "text-[#FF444A] underline" : ""
-            }>Donation</NavLink>
-          </li>
-          <li>
-            <NavLink
-            to="/statistics"
-            className={({ isActive, isPending }) =>
-              isPending ? "pending" : isActive ? "text-[#FF444A] underline" : ""
-            }>Statistics</NavLink>
-          </li>
-        </ul>
+    <nav className="mt-4 md:mt-6 lg:mt-12">
+      <div className="flex justify-between items-center container mx-auto px-6">
+        <div className="dropdown lg:hidden">
+          <label tabIndex={0} className="text-2xl">
+            <BsViewList></BsViewList>
+          </label>
+          <ul
+            tabIndex={0}
+            className="dropdown-content mt-3 z-[1] p-4 shadow bg-base-100 rounded-box w-40
+              md:w-52 font-medium"
+          >
+            {/* HOME  */}
+            <li>
+              <NavLink
+                to="/"
+                className={({ isActive, isPending }) =>
+                  isPending
+                    ? "pending"
+                    : isActive
+                    ? "text-[#FF444A] underline"
+                    : ""
+                }
+              >
+                Home
+              </NavLink>
+            </li>
+            {/* DONATION  */}
+            <li>
+              <NavLink
+                to="/donation"
+                className={({ isActive, isPending }) =>
+                  isPending
+                    ? "pending"
+                    : isActive
+                    ? "text-[#FF444A] underline"
+                    : ""
+                }
+              >
+                Donation
+              </NavLink>
+            </li>
+            {/* STATISTICS  */}
+            <li>
+              <NavLink
+                to="/statistics"
+                className={({ isActive, isPending }) =>
+                  isPending
+                    ? "pending"
+                    : isActive
+                    ? "text-[#FF444A] underline"
+                    : ""
+                }
+              >
+                Statistics
+              </NavLink>
+            </li>
+          </ul>
+        </div>
+        <div>
+          <Logo></Logo>
+        </div>
+        <div className="navbar-center hidden lg:flex">
+          <ul className="flex gap-12 font-bold text-xl">
+            {/* HOME  */}
+            <li>
+              <NavLink
+                to="/"
+                className={({ isActive, isPending }) =>
+                  isPending
+                    ? "pending"
+                    : isActive
+                    ? "text-[#FF444A] underline"
+                    : ""
+                }
+              >
+                Home
+              </NavLink>
+            </li>
+            {/* DONATION  */}
+            <li>
+              <NavLink
+                to="/donation"
+                className={({ isActive, isPending }) =>
+                  isPending
+                    ? "pending"
+                    : isActive
+                    ? "text-[#FF444A] underline"
+                    : ""
+                }
+              >
+                Donation
+              </NavLink>
+            </li>
+            {/* STATISTICS  */}
+            <li>
+              <NavLink
+                to="/statistics"
+                className={({ isActive, isPending }) =>
+                  isPending
+                    ? "pending"
+                    : isActive
+                    ? "text-[#FF444A] underline"
+                    : ""
+                }
+              >
+                Statistics
+              </NavLink>
+            </li>
+          </ul>
+        </div>
       </div>
     </nav>
   );
