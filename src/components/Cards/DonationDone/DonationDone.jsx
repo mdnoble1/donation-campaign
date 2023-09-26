131f9d9b806f7accdd886cba18ed178758fc791c
Md Noble <mdnoble.wd@gmail.com>
2023-09-26 19:51:40 +0600

    updated media queries on donation page

--- src/components/Cards/DonationDone/DonationDone.jsx
+++ src/components/Cards/DonationDone/DonationDone.jsx
@@ -17,17 +17,17 @@ const DonationDone = ({ donatedItem }) => {
           <div>
             <img src={description_image} alt="" />
           </div>
-          <div className="mt-3 md:mt-6 lg:mt-6 ml-2 md:ml-6 lg:ml-6">
+          <div className="mt-7 md:mt-6 lg:mt-6 ml-2 md:ml-6 lg:ml-6 space-y-4 md:space-y-2 lg:space-y-2">
             <h2
-              className={`bg-blue-300 py-1 px-4 text-sm md:text-base lg:text-base text-blue-800 font-semibold w-fit rounded-lg`}
+              className={`bg-blue-300 py-1 px-4 text-xs md:text-base lg:text-base text-blue-800 font-semibold w-fit rounded-lg`}
             >
               {category}
             </h2>
-            <h2 className={`mt-2 font-semibold text-base md:text-xl lg:text-xl text-blue-800`}>
+            <h2 className={`mt-2 font-semibold text-sm md:text-xl lg:text-xl text-blue-800`}>
               {title}
             </h2>
-            <h3 className="text-blue-800 text-sm md:text-base lg:text-base font-semibold mt-2">${price}</h3>
-            <button className="bg-blue-800 px-3 lg:px-6 py-2 lg:py-4 rounded-lg hover:bg-blue-400 font-semibold text-sm md:text-base lg:text-base text-white mt-2">
+            <h3 className="text-blue-800 text-xs md:text-base lg:text-base font-semibold mt-2">${price}</h3>
+            <button className="bg-blue-800 px-3 lg:px-6 py-2 lg:py-4 rounded-lg hover:bg-blue-400 font-semibold text-xs md:text-base lg:text-base text-white mt-2">
               View Details
             </button>
           </div>
