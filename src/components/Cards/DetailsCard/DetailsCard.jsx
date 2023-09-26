0da25d8c6221785026a3e2e6ead74ceb4c9c3fea
Md Noble <mdnoble.wd@gmail.com>
2023-09-26 08:25:03 +0600

    updated donation details cards media queries

--- src/components/Cards/DetailsCard/DetailsCard.jsx
+++ src/components/Cards/DetailsCard/DetailsCard.jsx
@@ -11,11 +11,11 @@ const DetailsCard = ({ donationDetail }) => {
             <div className="relative">
                 <img className="rounded-lg w-11/12 lg:w-full mx-auto" src={detail_image} alt="" />
                 <div className="bg-black w-11/12 lg:w-full h-[80px] md:h-[110px] lg:h-[160px] rounded-b-lg absolute bottom-[1px] opacity-60 left-[17px] md:left-8 lg:left-0"></div>
-                <button className="btn btn-primary font-semibold text-xl text-white absolute bottom-14 left-20">Donate ${price}</button>
+                <button className="bg-blue-600 px-4 lg:px-6 py-2 lg:py-4 rounded-lg hover:bg-blue-400 font-semibold lg:text-xl text-white absolute bottom-5 md:bottom-8 lg:bottom-14 left-10 md:left-14 lg:left-20">Donate ${price}</button>
             </div>
             <div className="text-center lg:text-left">
                 <h2 className="font-bold text-2xl lg:text-4xl text-black mt-8">{title}</h2>
-                <p className="w-11/12 lg:max-w-7xl text-gray-600 lg:text-lg mt-4 text-center lg:text-justify mx-auto lg:mx-0">{description}</p>
+                <p className="w-11/12 lg:w-full text-gray-600 lg:text-lg mt-4 text-center lg:text-justify mx-auto lg:mx-0">{description}</p>
             </div>
         </div>
     </div>
