3d851d9520bd441748bff3190142d29c48c3b9f6
Md Noble <mdnoble.wd@gmail.com>
2023-09-27 08:58:07 +0600

    added colors on donation page cards dynamically

--- src/components/Cards/DonationDone/DonationDone.jsx
+++ src/components/Cards/DonationDone/DonationDone.jsx
@@ -11,23 +11,28 @@ const DonationDone = ({ donatedItem }) => {
     price,
   } = donatedItem;
 
+  console.log(category_bg,
+    card_bg,
+    text_color)
+
   return (
     <div className="flex justify-center">
-        <div className="flex md:gap-6 lg:gap-6 rounded-xl bg-blue-100 drop-shadow-xl w-11/12 lg:w-full">
+        <div className="flex md:gap-6 lg:gap-6 rounded-xl drop-shadow-xl w-11/12 lg:w-full" style = {{background:card_bg}}>
           <div>
             <img src={description_image} alt="" />
           </div>
-          <div className="mt-7 md:mt-6 lg:mt-6 ml-2 md:ml-6 lg:ml-6 space-y-4 md:space-y-2 lg:space-y-2">
+          <div className="mt-7 md:mt-6 lg:mt-6 ml-2 md:ml-6 lg:ml-6 space-y-4 md:space-y-2 lg:space-y-2" >
             <h2
-              className={`bg-blue-300 py-1 px-4 text-xs md:text-base lg:text-base text-blue-800 font-semibold w-fit rounded-lg`}
+              className={`py-1 px-4 text-xs md:text-base lg:text-base font-semibold w-fit rounded-lg`}
+              style = {{background:category_bg , color:text_color}}
             >
               {category}
             </h2>
-            <h2 className={`mt-2 font-semibold text-sm md:text-xl lg:text-xl text-blue-800`}>
+            <h2 className={`mt-2 font-semibold text-sm md:text-xl lg:text-xl`} style = {{color:text_color}}>
               {title}
             </h2>
-            <h3 className="text-blue-800 text-xs md:text-base lg:text-base font-semibold mt-2">${price}</h3>
-            <button className="bg-blue-800 px-3 lg:px-6 py-2 lg:py-4 rounded-lg hover:bg-blue-400 font-semibold text-xs md:text-base lg:text-base text-white mt-2">
+            <h3 className="text-xs md:text-base lg:text-base font-semibold mt-2" style = {{color:text_color}}>${price}</h3>
+            <button className="px-3 lg:px-6 py-2 lg:py-4 rounded-lg hover:opacity-60 font-semibold text-xs md:text-base lg:text-base text-white mt-2" style = {{background:text_color}}>
               View Details
             </button>
           </div>
